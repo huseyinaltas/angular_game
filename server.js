3ad8da8d4b61556558cbb74d2bce1398a4cbb408
Huseyin Altas <altas2001@hotmail.com>
2020-11-01 17:01:21 -0600

    Heroku updated

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
+
+
 //Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + './dist/game-number'));
 
-app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+ '/src/index.html'));
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
+// // Instruct the app
+// // to use the forceSSL
+// // middleware
+// app.use(forceSSL());
+
+
+// Serve static files....
+app.use(express.static(__dirname + '/game-number/src/'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
+
 app.listen(process.env.PORT || 8080);
